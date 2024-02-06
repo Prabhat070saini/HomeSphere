ce1071a3b3529f16d1559b097228faf331a5bcea
Prabhat <Prabhatsaini070@gmail.com>
2024-02-06 20:06:53 +0530

    inital commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SingOut from "./pages/SingOut";
+import Singin from "./pages/Singin";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import About from "./pages/About";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/singin" element={<Singin />} />
+        <Route path="/sing-out" element={<SingOut />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
