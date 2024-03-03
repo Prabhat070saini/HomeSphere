407add61646eeea92599dc9471bfe8c53fdde475
Prabhat <Prabhatsaini070@gmail.com>
2024-03-04 01:06:34 +0530

    create search funtionality api

--- Server/routes/Listing.route.js
+++ Server/routes/Listing.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createListing, deleteListing, updateListing, getList } = require('../controllers/listing.controller');
+const { createListing, deleteListing, updateListing, getList, getListings } = require('../controllers/listing.controller');
 const { verifyToken } = require('../utils/verifyUser');
 const router = express.Router();
 
@@ -8,4 +8,5 @@ router.post(`/create`, verifyToken, createListing);
 router.delete(`/delete/:id`, verifyToken, deleteListing)
 router.post(`/update/:id`, verifyToken, updateListing)
 router.get(`/getlist/:id`, getList);
+router.get(`/getlistings`, getListings);
 module.exports = router
